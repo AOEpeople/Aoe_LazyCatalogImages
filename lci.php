05de54cc74dbf5a6d64eca0b5b9b965f1ceafe3b
Lee Saferite <lee.saferite@aoe.com>
2015-03-26 02:13:46 +0000

    Switch from hack to intercept LCI image requests
    The original method wasn't actually working correctly due to the get.php script limiting loaded modules to 'Mage_core' only.
    This new method should work now without any hacks needed.

---
+++ lci.php
@@ -0,0 +1,69 @@
+<?php
+if (version_compare(phpversion(), '5.2.0', '<') === true) {
+    exit;
+}
+
+error_reporting(E_ALL | E_STRICT);
+define('MAGENTO_ROOT', getcwd());
+
+$compilerConfig = MAGENTO_ROOT . '/includes/config.php';
+if (file_exists($compilerConfig)) {
+    include $compilerConfig;
+}
+
+$maintenanceFile = 'maintenance.flag';
+if (file_exists($maintenanceFile)) {
+    include_once dirname(__FILE__) . '/errors/503.php';
+    exit;
+}
+
+$mageFilename = MAGENTO_ROOT . '/app/Mage.php';
+if (!file_exists($mageFilename)) {
+    exit;
+}
+require_once $mageFilename;
+
+if (isset($_SERVER['MAGE_IS_DEVELOPER_MODE'])) {
+    Mage::setIsDeveloperMode(true);
+}
+
+umask(0);
+
+$mageRunCode = isset($_SERVER['MAGE_RUN_CODE']) ? $_SERVER['MAGE_RUN_CODE'] : '';
+$mageRunType = isset($_SERVER['MAGE_RUN_TYPE']) ? $_SERVER['MAGE_RUN_TYPE'] : 'store';
+Mage::init($mageRunCode, $mageRunType);
+
+try {
+    /** @var Aoe_LazyCatalogImages_Helper_Catalog_Image $imageHelper */
+    $imageHelper = Mage::helper('Aoe_LazyCatalogImages/Catalog_Image');
+    if ($imageHelper->initFromToken(pathinfo(Mage::app()->getRequest()->getPathInfo(), PATHINFO_FILENAME))) {
+        $cacheAge = $imageHelper->getMaxCacheAge();
+        try {
+            /** @var Aoe_LazyCatalogImages_Model_HttpTransferAdapter $adapter */
+            $adapter = Mage::getModel('Aoe_LazyCatalogImages/HttpTransferAdapter');
+            $adapter->send(
+                array(
+                    'filepath' => $imageHelper->getOutputFile(),
+                    'headers'  => array(
+                        'Cache-Control' => 'public, max-age=' . $cacheAge
+                    )
+                )
+            );
+            exit;
+        } catch (Exception $e) {
+            Mage::logException($e);
+            Mage::app()->getResponse()
+                ->setRedirect(Mage::getDesign()->getSkinUrl($imageHelper->getPlaceholder()))
+                ->setHeader('Cache-Control', 'public, max-age=' . intval($cacheAge / 10))
+                ->sendResponse();
+            exit;
+        }
+    } else {
+        Mage::app()->getResponse()
+            ->setHttpResponseCode(404)
+            ->sendResponse();
+        exit;
+    }
+} catch (Exception $e) {
+    Mage::logException($e);
+}
