bdabed639087e91e8005549cb226f986029f43ea
Lee Saferite <lee.saferite@aoe.com>
2015-04-10 18:32:09 +0000

    Allow setting of the _newFile variable

--- app/code/community/Aoe/LazyCatalogImages/Model/Catalog/Product/Image.php
+++ app/code/community/Aoe/LazyCatalogImages/Model/Catalog/Product/Image.php
@@ -9,4 +9,15 @@ class Aoe_LazyCatalogImages_Model_Catalog_Product_Image extends Mage_Catalog_Mod
     {
         return (bool)$this->_isBaseFilePlaceholder;
     }
+
+    /**
+     * @param string $file
+     *
+     * @return $this
+     */
+    public function setNewFile($file)
+    {
+        $this->_newFile = $file;
+        return $this;
+    }
 }
